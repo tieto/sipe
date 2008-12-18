874aa6e968f6532f5d1d3b0441f993a8e0dd46b7
Stefan Becker <stefan.becker@nokia.com>
2008-12-18 09:52:06 +0200

    Fix error when compiling against libpurple < 2.4
    gc->last_received was added to the API in 2.4.0. Wrapped the usage of it in
    sipe_keep_alive() with a version check. RHEL5 users are happy again :-)

--- src/sipe.c
+++ src/sipe.c
@@ -164,8 +164,11 @@ static void sipe_keep_alive(PurpleConnection *gc)
 	} else {
 		time_t now = time(NULL);
 		if ((sip->keepalive_timeout > 0) &&
-		    ((now - sip->last_keepalive) >= sip->keepalive_timeout) &&
-		    ((now - gc->last_received) >= sip->keepalive_timeout)) {
+		    ((now - sip->last_keepalive) >= sip->keepalive_timeout)
+#if PURPLE_VERSION_CHECK(2,4,0)
+		    && ((now - gc->last_received) >= sip->keepalive_timeout)
+#endif
+		    ) {
 			purple_debug_info("sipe", "sending keep alive\n");
 			sendout_pkt(gc, "\r\n\r\n");
 			sip->last_keepalive = now;
