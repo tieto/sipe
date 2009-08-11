c77126909ac9fb1b6dc0d36851f9005d6d1e5051
Stefan Becker <stefan.becker@nokia.com>
2009-08-11 22:20:46 +0300

    Fix previous commit: make sure timeout is never 0...
    *Sigh* never do math late at night :-(

--- src/sipe.c
+++ src/sipe.c
@@ -1847,7 +1847,7 @@ static void sipe_buddy_subscribe_cb(SIPE_UNUSED_PARAMETER char *name, struct sip
 	gchar *action_name = g_strdup_printf(ACTION_NAME_PRESENCE, buddy->name);
 	/* g_hash_table_size() can never return 0, otherwise this function wouldn't be called :-) */
 	guint time_range = (g_hash_table_size(sip->buddies) * 1000) / 25; /* time interval for 25 requests per sec. In msec. */
-	guint timeout = ((guint) rand()) / (RAND_MAX / time_range); /* random period within the range */
+	guint timeout = ((guint) rand()) / (RAND_MAX / time_range) + 1; /* random period within the range but never 0! */
 	sipe_schedule_action_msec(action_name, timeout, sipe_subscribe_presence_single, g_free, sip, g_strdup(buddy->name));
 }
 
