tieto/sipe

commit 0b738bfe7af33121bc6a1205dc85eb9226f929f0

Fix type errors in sip-sec-ntlm.c reported in forum

src/sip-sec-ntlm.c
@@ -187,7 +187,7 @@ static void des_ecb_encrypt(const unsigned char *plaintext, unsigned char *resul
 {
 	PurpleCipher *cipher;
 	PurpleCipherContext *context;
-	gsize outlen;
+	size_t outlen;
 
 	cipher = purple_ciphers_find_cipher("des");
 	context = purple_cipher_context_new(cipher, NULL);
@@ -202,8 +202,8 @@ unicode_strconvcopy(gchar *dest, const gchar *source, int remlen)
 	GIConv fd;
 	gchar *inbuf = (gchar *) source;
 	gchar *outbuf = dest;
-	size_t inbytes = strlen(source);
-	size_t outbytes = remlen;
+	gsize inbytes = strlen(source);
+	gsize outbytes = remlen;
 #ifdef HAVE_LANGINFO_CODESET
 	char *sys_cp = nl_langinfo(CODESET);
 #else
@@ -440,7 +440,7 @@ purple_ntlm_gen_signature (const char * buf, unsigned char * signing_key, guint3
 	gchar signature [33];
 	int i, j;
 	PurpleCipherContext *rc4 = purple_cipher_context_new_by_name("rc4", NULL);
-	purple_cipher_context_set_option(rc4,"key_len", GINT_TO_POINTER(key_len));
+	purple_cipher_context_set_option(rc4, "key_len", (gpointer) key_len);
 
 	purple_cipher_context_set_key(rc4, signing_key);
 	purple_cipher_context_encrypt(rc4, (const guchar *)plaintext, 12, result+4, NULL);

commit cdc56b311c901c98cb87d13053e3b044345f1da8

Another stab at the type errors in sip-sec-ntlm.c
Commit 0b738bfe7af33121bc6a1205dc85eb9226f929f0 broke x86_64 compilation.

src/sip-sec-ntlm.c
@@ -284,7 +284,7 @@ static void
 RC4K (const unsigned char * k, const unsigned char * d, unsigned char * result)
 {
 	PurpleCipherContext * context = purple_cipher_context_new_by_name("rc4", NULL);
-	purple_cipher_context_set_option(context, "key_len", (gpointer)16);
+	purple_cipher_context_set_option(context, "key_len", GUINT_TO_POINTER(16));
 	purple_cipher_context_set_key(context, k);
 	purple_cipher_context_encrypt(context, (const guchar *)d, 16, result, NULL);
 	purple_cipher_context_destroy(context);
@@ -431,7 +431,7 @@ CRC32 (const char * msg)
 }
 
 static gchar *
-purple_ntlm_gen_signature (const char * buf, unsigned char * signing_key, guint32 random_pad, long sequence, int key_len)
+purple_ntlm_gen_signature (const char * buf, unsigned char * signing_key, guint32 random_pad, long sequence, unsigned long key_len)
 {
 	gint32 *res_ptr;
 	gint32 plaintext [] = {0, CRC32(buf), sequence};
@@ -440,7 +440,7 @@ purple_ntlm_gen_signature (const char * buf, unsigned char * signing_key, guint3
 	gchar signature [33];
 	int i, j;
 	PurpleCipherContext *rc4 = purple_cipher_context_new_by_name("rc4", NULL);
-	purple_cipher_context_set_option(rc4, "key_len", (gpointer) key_len);
+	purple_cipher_context_set_option(rc4, "key_len", GUINT_TO_POINTER(key_len));
 
 	purple_cipher_context_set_key(rc4, signing_key);
 	purple_cipher_context_encrypt(rc4, (const guchar *)plaintext, 12, result+4, NULL);