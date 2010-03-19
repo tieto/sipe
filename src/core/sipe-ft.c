3b32872129167612bdf5f30ea177f9fd719e35a5
Stefan Becker <stefan.becker@nokia.com>
2010-03-19 18:13:53 +0200

    configure: add check for sys/sockio.h
    Required for SIOCGIFCONF on Solaris. Reported by psfales.

--- src/core/sipe-ft.c
+++ src/core/sipe-ft.c
@@ -50,6 +50,9 @@
 #include <netinet/in.h>
 #include <net/if.h>
 #include <arpa/inet.h>
+#ifdef HAVE_SYS_SOCKIO_H
+#include <sys/sockio.h> /* SIOCGIFCONF for Solaris */
+#endif
 #endif
 
 #define SIPE_FT_KEY_LENGTH          24
