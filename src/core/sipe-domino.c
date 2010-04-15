1d1e2b2585c6f3d64f39c484c137420743d2e4b8
pier11 <pier11@operamail.com>
2010-04-15 14:59:02 +0100

    domino: login reguest We've log in successfully!
    HTTP/1.1 200 OK
    Server: Lotus-Domino
    Date: Thu, 15 Apr 2010 13:57:54 GMT
    Expires: Tue, 01 Jan 1980 06:00:00 GMT
    Content-Type: text/html; charset=UTF-8
    Content-Length: 4029
    Cache-control: no-cache

--- src/core/sipe-domino.c
+++ src/core/sipe-domino.c
@@ -31,7 +31,7 @@ Server requirements: Domino 5.0.2 and above with Web Access.
 1) Authenticates to server (HTTPS POST, plaintext login/password over SSL)
 https://[domino_server]/[databasename].nsf/?Login
 Content-Type=application/x-www-form-urlencoded
-Username=[email]&Password=[password] (email is url-encoded) 
+Username=[email]&Password=[password] (params are url-encoded)
 Saves auth cookie.
 Set-Cookie=DomAuthSessId=17D0428F7B9D57D4D0B064AE42FD21F9; path=/
 
@@ -52,16 +52,89 @@ Similar functionality for iCalendar/CalDAV/Google would be great to implement to
 
 #include <glib.h>
 
+/* @TODO replace purple_url_encode() with non-purple equiv. */
+#include "util.h"
+
 #include "sipe-core.h"
 #include "sipe.h"
 #include "sipe-backend.h"
 #include "sipe-cal.h"
 #include "sipe-domino.h"
+#include "http-conn.h"
+
+/**
+ * GetUserAvailabilityRequest SOAP request to Exchange Web Services
+ * to obtain our Availability (FreeBusy, WorkingHours, Meetings) information.
+ * @param email      (%s) Ex.: alice@cosmo.local
+ * @param start_time (%s) Ex.: 2009-12-06T00:00:00
+ * @param end_time   (%s) Ex.: 2009-12-09T23:59:59
+ */
+#define SIPE_DOMINO_LOGIN_REQUEST \
+"Username=%s&Password=%s"
+
+static void
+sipe_domino_process_login_response(int return_code,
+				   const char *body,
+				   HttpConn *conn,
+				   void *data)
+{
+	struct sipe_calendar *cal = data;
+
+	SIPE_DEBUG_INFO_NOFORMAT("sipe_domino_process_login_response: cb started.");
+
+	http_conn_set_close(conn);
+	cal->http_conn = NULL;
+
+	if (return_code == 200 && body) {
+		SIPE_DEBUG_INFO_NOFORMAT("sipe_domino_process_login_response: SUCCESS");
+	} else if (return_code < 0) {
+		SIPE_DEBUG_INFO("sipe_domino_process_login_response: FAILURE, ret=%d", return_code);
+		cal->http_conn = NULL;
+	}
+}
 
 static void
 sipe_domino_do_login_request(struct sipe_calendar *cal)
 {
-	/* still unused */(void) cal;
+	if (cal->as_url) {
+		char *body;
+		const char *content_type = "application/x-www-form-urlencoded";
+		char *login_url = g_strconcat(cal->as_url, "/?Login", NULL);
+		char *user;
+		char *password;
+
+		SIPE_DEBUG_INFO_NOFORMAT("sipe_domino_do_login_request: going Login req.");
+		
+		if (!cal->auth) return;
+		
+		/* @TODO replace purple_url_encode() with non-purple equiv. */
+		user     = g_strdup(purple_url_encode(cal->auth->user));
+		password = g_strdup(purple_url_encode(cal->auth->password));
+
+		body = g_strdup_printf(SIPE_DOMINO_LOGIN_REQUEST, user, password);
+		g_free(user);
+		g_free(password);
+		
+		if (!cal->http_conn) {
+			cal->http_conn = http_conn_create(cal->account,
+							  HTTP_CONN_SSL,
+							  login_url,
+							  body,
+							  content_type,
+							  cal->auth,
+							  sipe_domino_process_login_response,
+							  cal);
+		} else {
+			http_conn_post(cal->http_conn,
+				       login_url,
+				       body,
+				       content_type,
+				       sipe_domino_process_login_response,
+				       cal);
+		}
+		g_free(login_url);
+		g_free(body);
+	}
 }
 
 void
