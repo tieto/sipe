fdae6bba784fea64998a9aaaee67e24aad3154f2
Stefan Becker <stefan.becker@nokia.com>
2011-01-20 06:31:07 +0200

    Fix #3161273: Lost Connection Gives No Error Message (part II)
    Implement response handling for IM INFO/KeyboardActivity SIP messages.
    
    If we receive a 408/480/481 response then we assume a dangling IM session. We
    sent out a BYE for the dialog and drop it.

--- src/core/sipe-user.c
+++ src/core/sipe-user.c
@@ -22,11 +22,39 @@
 
 #include <glib.h>
 
+#include "sipe-common.h"
+#include "sipmsg.h"
 #include "sip-transport.h"
+#include "sipe-backend.h"
 #include "sipe-core.h"
 #include "sipe-core-private.h"
 #include "sipe-dialog.h"
 #include "sipe-session.h"
+#include "sipe-utils.h"
+
+static gboolean process_info_typing_response(struct sipe_core_private *sipe_private,
+					     struct sipmsg *msg,
+					     SIPE_UNUSED_PARAMETER struct transaction *trans)
+{
+	/* Indicates dangling IM session which needs to be dropped */
+	if (msg->response == 408 || /* Request timeout */
+	    msg->response == 480 || /* Temporarily Unavailable */
+	    msg->response == 481) { /* Call/Transaction Does Not Exist */
+		gchar *with = parse_from(sipmsg_find_header(msg, "To"));
+		struct sip_session *session = sipe_session_find_im(sipe_private, with);
+		struct sip_dialog *dialog = sipe_dialog_find(session, with);
+		if (dialog) {
+			SIPE_DEBUG_INFO_NOFORMAT("process_info_typing_response: assuming dangling IM session, dropping it.");
+			sip_transport_bye(sipe_private, dialog);
+
+			/* We might not get a valid reply to our BYE,
+			   so make sure the dialog is removed for sure. */
+			sipe_dialog_remove(session, with);
+		}
+		g_free(with);
+	}	
+	return(TRUE);
+}
 
 void sipe_core_user_feedback_typing(struct sipe_core_public *sipe_public,
 				    const gchar *to)
@@ -43,7 +71,7 @@ void sipe_core_user_feedback_typing(struct sipe_core_public *sipe_public,
 				   "<status status=\"type\" />"
 				   "</KeyboardActivity>",
 				   dialog,
-				   NULL);
+				   process_info_typing_response);
 	}
 }
 
