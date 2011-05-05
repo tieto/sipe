ec3a8ba61c6801505780fd079486e6c1150a9f3d
Stefan Becker <stefan.becker@nokia.com>
2011-05-05 23:28:08 +0300

    purple: make it compile against 2.8.x API
    Tested with latest im.pidgin.pidgin snapshot.

--- src/purple/purple-plugin.c
+++ src/purple/purple-plugin.c
@@ -521,6 +521,10 @@ static PurplePluginProtocolInfo sipe_prpl_info =
 	NULL,					/* get_moods */
 	NULL,					/* set_public_alias */
 	NULL,					/* get_public_alias */
+#if PURPLE_VERSION_CHECK(2,8,0)
+	NULL,					/* add_buddy_with_invite */
+	NULL,					/* add_buddies_with_invite */
+#endif
 #endif
 #endif
 #endif
