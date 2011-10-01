41e60ec7b020041778ca2d778d62c9a00cd883b6
Stefan Becker <chemobejk@gmail.com>
2011-10-01 21:37:13 +0300

    OBS: add NSIS template for mingw builds
    This is the first step to integrate generation of a NSIS installer package into mingw builds.
    
    Add a .nsi template plus a Perl script that generates the real .nsi files using the po/LINGUAS
    file to detect the currently supported languages.

---
+++ contrib/opensuse-build-service/generate_nsi.pl
@@ -0,0 +1,37 @@
+#!/usr/bin/perl
+use 5.008;
+use strict;
+use warnings;
+
+# check commandline arguments
+die "Usage: $0 <LINGUAS file> < nsi-template > nsi-output\n"
+    if @ARGV < 1;
+
+# process LINGUAS file
+open(my $fh, "<", $ARGV[0])
+    or die "$0: can't open LINGUAS file '$ARGV[0]': $!\n";
+my %languages = map { ($_, 1) } map { chomp; s/^\s+//; s/\s+$//; $_ } <$fh>;
+close($fh)
+    or die "$0: error while reading LINGUAS file '$ARGV[0]': $!\n";
+print STDERR "Found ", scalar(keys %languages), " language(s): ",
+                       join(" ", sort keys %languages), "\n";
+
+# read .nsi template from STDIN
+# write .nsi file to STDOUT
+while (<STDIN>) {
+    if (/^;;; INSTALL_FILES_LOCALE/) {
+	print map({
+	           ("File \"\${MINGW_ROOT}\\share\\locale\\ar\\LC_MESSAGES\\pidgin-sipe.mo\"\n",
+		    "SetOutPath \"\$INSTDIR\\locale\\cs\\LC_MESSAGES\"\n")
+		  } sort keys %languages);
+    } elsif (/^;;; DELETE_FILES_LOCALE/) {
+	print map({
+	           "Delete \"\$INSTDIR\\locale\\ar\\LC_MESSAGES\\pidgin-sipe.mo\"\n"
+		  } sort keys %languages);
+    } else {
+	print;
+    }
+}
+
+# That's all folks...
+exit 0;
