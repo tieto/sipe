5715943b20f68ca8ddbfbae28d721d6b126b9a5f
Stefan Becker <chemobejk@gmail.com>
2012-03-21 17:58:42 +0200

    purple: build fix for *BSD
    Finally got a working FreeBSD9.0 VM where I can compile the code. This
    revealed that sys/socket.h is needed for purple-dnsquery.c. Let's hope
    that that is enough also for all other *BSDs.

--- src/purple/purple-dnsquery.c
+++ src/purple/purple-dnsquery.c
@@ -3,7 +3,7 @@
  *
  * pidgin-sipe
  *
- * Copyright (C) 2010 SIPE Project <http://sipe.sourceforge.net/>
+ * Copyright (C) 2010-12 SIPE Project <http://sipe.sourceforge.net/>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -26,6 +26,7 @@
 #else
 #include <arpa/inet.h>
 #include <netinet/in.h>
+#include <sys/socket.h>
 #endif
 
 #include <glib.h>
