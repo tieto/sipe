36b8ece34105dfe611cd2902f57b3f712a7b9dd5
Stefan Becker <chemobejk@gmail.com>
2012-08-22 12:05:56 +0300

    telepathy: initial dummy code
    Something which compiles & links but doesn't do anything yet.

---
+++ src/telepathy/telepathy-main.c
@@ -0,0 +1,39 @@
+/**
+ * @file telepathy-main.c
+ *
+ * pidgin-sipe
+ *
+ * Copyright (C) 2012 SIPE Project <http://sipe.sourceforge.net/>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	(void)argv;
+	return(0);
+}
+
+/*
+  Local Variables:
+  mode: c
+  c-file-style: "bsd"
+  indent-tabs-mode: t
+  tab-width: 8
+  End:
+*/
