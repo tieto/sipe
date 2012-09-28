tieto/sipe

commit faaad867f420a8c302aa0cc8324bab82183d1873

MSVC compatibility for Instantbird.

src/purple/purple-network.c
@@ -47,7 +47,9 @@
 #endif
 #endif
 
+#ifdef HAVE_UNISTD
 #include <unistd.h>
+#endif
 
 #include "sipe-common.h"
 #include "sipe-backend.h"

src/purple/purple-transport.c
@@ -27,7 +27,9 @@
 #include <errno.h>
 #include <string.h>
 #include <time.h>
+#ifdef HAVE_UNISTD
 #include <unistd.h>
+#endif
 
 #include <glib.h>
 

commit 61bd637854c0a9412b2ec5c35ac88d4ba4538332

purple: fix build break
Fix broken flagging introduced in

   commit faaad867f420a8c302aa0cc8324bab82183d1873

src/purple/purple-media.c
@@ -28,7 +28,9 @@
 #include "glib/gstdio.h"
 #include <fcntl.h>
 #include <string.h>
+#ifdef HAVE_UNISTD_H
 #include <unistd.h>
+#endif
 
 #include "sipe-common.h"
 

src/purple/purple-network.c
@@ -47,7 +47,7 @@
 #endif
 #endif
 
-#ifdef HAVE_UNISTD
+#ifdef HAVE_UNISTD_H
 #include <unistd.h>
 #endif
 

src/purple/purple-transport.c
@@ -3,7 +3,7 @@
  *
  * pidgin-sipe
  *
- * Copyright (C) 2010 SIPE Project <http://sipe.sourceforge.net/>
+ * Copyright (C) 2010-12 SIPE Project <http://sipe.sourceforge.net/>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -27,7 +27,7 @@
 #include <errno.h>
 #include <string.h>
 #include <time.h>
-#ifdef HAVE_UNISTD
+#ifdef HAVE_UNISTD_H
 #include <unistd.h>
 #endif
 