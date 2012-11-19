8d711b274b5d528459a783966e0a9af185b26b55
Stefan Becker <chemobejk@gmail.com>
2012-11-19 19:10:53 +0200

    mingw: update fetch script to Pidgin 2.10.6

--- contrib/mingw-cross-compile/fetch.sh
+++ contrib/mingw-cross-compile/fetch.sh
@@ -7,7 +7,7 @@
 # Check this page for latest MinGW/Pidgin URLs if you get fetch errors!
 #
 # update Pidgin version here
-export PIDGIN_VERSION=2.10.5
+export PIDGIN_VERSION=2.10.6
 
 # must be absolute path
 export PIDGIN_DEV_ROOT=$(pwd -P)/build-${PIDGIN_VERSION}
