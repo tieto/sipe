tieto/sipe

commit 5fdf3728c82a23b07e7eeb80f9c1d4cee38f1511

transport: remove special handling for NTLM
sip-sec.c now returns an empty string for the initial step instead.
Internalize connection-less NTLM special case handling into SSPI code.

src/core/sip-sec-ntlm.c
@@ -1694,12 +1694,12 @@ typedef struct _context_ntlm {
 	char* domain;
 	char *username;
 	char *password;
-	int step;
 	guchar *client_sign_key;
 	guchar *server_sign_key;
 	guchar *client_seal_key;
 	guchar *server_seal_key;
 	guint32 flags;
+	gboolean initial;
 } *context_ntlm;
 
 
@@ -1718,6 +1718,7 @@ sip_sec_acquire_cred__ntlm(SipSecContext context,
 	ctx->domain   = g_strdup(domain);
 	ctx->username = g_strdup(username);
 	ctx->password = g_strdup(password);
+	ctx->initial  = TRUE;
 
 	return SIP_SEC_E_OK;
 }
@@ -1732,17 +1733,18 @@ sip_sec_init_sec_context__ntlm(SipSecContext context,
 
 	SIPE_DEBUG_INFO_NOFORMAT("sip_sec_init_sec_context__ntlm: in use");
 
-	ctx->step++;
-	if (ctx->step == 1) {
-		if (!context->is_connection_based) {
-			out_buff->length = 0;
-			out_buff->value = NULL;
-		} else {
+	if (ctx->initial) {
+		ctx->initial = FALSE;
+
+		/* HTTP */
+		if (context->is_connection_based) {
 			sip_sec_ntlm_gen_negotiate(out_buff);
 			sip_sec_ntlm_message_describe(out_buff, "Negotiate");
+		/* SIP */
+		} else {
+			out_buff->length = 0;
+			out_buff->value = NULL;
 		}
-		return SIP_SEC_I_CONTINUE_NEEDED;
-
 	} else 	{
 		sip_uint32 res;
 		guchar *client_sign_key = NULL;
@@ -1815,9 +1817,9 @@ sip_sec_init_sec_context__ntlm(SipSecContext context,
 
 		/* Authentication is completed */
 		ctx->common.is_ready = TRUE;
-
-		return SIP_SEC_E_OK;
 	}
+
+	return SIP_SEC_E_OK;
 }
 
 /**

src/core/sip-sec-sspi.c
@@ -54,6 +54,7 @@ typedef struct _context_sspi {
 	CtxtHandle* ctx_sspi;
 	/** Kerberos or NTLM */
 	const char *mech;
+	gboolean initial;
 } *context_sspi;
 
 static int
@@ -144,9 +145,41 @@ sip_sec_init_sec_context__sspi(SipSecContext context,
 	ULONG req_flags;
 	ULONG ret_flags;
 	context_sspi ctx = (context_sspi)context;
-	CtxtHandle* out_context = g_malloc0(sizeof(CtxtHandle));
+	gboolean connection_less_ntlm = !context->is_connection_based &&
+		ctx->mech && (strcmp(ctx->mech, SSPI_MECH_NTLM) == 0);
+	CtxtHandle* out_context;
 
 	SIPE_DEBUG_INFO_NOFORMAT("sip_sec_init_sec_context__sspi: in use");
+
+	if (connection_less_ntlm && ctx->initial) {
+
+		/* empty initial message for connection-less NTLM */
+		if (in_buff.value == NULL) {
+			SIPE_DEBUG_INFO_NOFORMAT("sip_sec_init_sec_context__sspi: initial message for connection-less NTLM");
+			out_buff->length = 0;
+			out_buff->value = NULL;
+			return SIP_SEC_E_OK;
+
+		/* call again to create context for connection-less NTLM */
+		} else {
+			SipSecBuffer empty = { 0, NULL };
+
+			ctx->initial = FALSE;
+			ret = sip_sec_init_sec_context__sspi(context,
+							     empty,
+							     out_buff,
+							     service_name);
+			if (ret == SEC_E_OK) {
+				SIPE_DEBUG_INFO_NOFORMAT("sip_sec_init_sec_context__sspi: connection-less NTLM second round");
+				g_free(out_buff->value);
+			} else {
+				sip_sec_sspi_print_error("sip_sec_init_sec_context__sspi: unexpected NTLM state", ret);
+				return SIP_SEC_E_INTERNAL_ERROR;
+			}
+		}
+	}
+
+	out_context = g_malloc0(sizeof(CtxtHandle));
 
 	input_desc.cBuffers = 1;
 	input_desc.pBuffers = &in_token;
@@ -170,9 +203,7 @@ sip_sec_init_sec_context__sspi(SipSecContext context,
 		     ISC_REQ_INTEGRITY |
 		     ISC_REQ_IDENTIFY);
 
-	if (ctx->mech && !strcmp(ctx->mech, SSPI_MECH_NTLM) &&
-	    !context->is_connection_based)
-	{
+	if (connection_less_ntlm) {
 		req_flags |= (ISC_REQ_DATAGRAM);
 	}
 
@@ -338,6 +369,7 @@ sip_sec_create_context__sspi(guint type)
 	context->mech = (type == SIPE_AUTHENTICATION_TYPE_NTLM) ? SSPI_MECH_NTLM :
 			((type == SIPE_AUTHENTICATION_TYPE_KERBEROS) ? SSPI_MECH_KERBEROS :
 			 ((type == SIPE_AUTHENTICATION_TYPE_NEGOTIATE) ? SSPI_MECH_NEGOTIATE : SSPI_MECH_TLS_DSK));
+	context->initial = TRUE;
 
 	return((SipSecContext) context);
 }

src/core/sip-sec.c
@@ -162,7 +162,7 @@ sip_sec_init_context_step(SipSecContext context,
 			if (out_buff.length > 0 && out_buff.value) {
 				*output_toked_base64 = g_base64_encode(out_buff.value, out_buff.length);
 			} else {
-				*output_toked_base64 = NULL;
+				*output_toked_base64 = g_strdup("");
 			}
 
 			g_free(out_buff.value);
@@ -193,7 +193,7 @@ sip_sec_init_context(SipSecContext *context,
 
 	*context = sip_sec_create_context(type,
 					  sso,
-					  0, /* Connectionless for SIP */
+					  FALSE, /* connection-less for SIP */
 					  domain,
 					  username,
 					  password);

src/core/http-conn.c
@@ -3,7 +3,7 @@
  *
  * pidgin-sipe
  *
- * Copyright (C) 2010-13 SIPE Project <http://sipe.sourceforge.net/>
+ * Copyright (C) 2010-2013 SIPE Project <http://sipe.sourceforge.net/>
  * Copyright (C) 2009 pier11 <pier11@operamail.com>
  *
  *
@@ -497,7 +497,7 @@ http_conn_process_input_message(HttpConn *http_conn,
 			http_conn->sec_ctx =
 				sip_sec_create_context(auth_type,
 						       use_sso,
-						       1,
+						       TRUE, /* connection-based for HTTP */
 						       http_conn->auth && http_conn->auth->domain ? http_conn->auth->domain : "",
 						       http_conn->auth ? http_conn->auth->user : NULL,
 						       http_conn->auth ? http_conn->auth->password : NULL);

src/core/sip-transport.c
@@ -3,7 +3,7 @@
  *
  * pidgin-sipe
  *
- * Copyright (C) 2010-12 SIPE Project <http://sipe.sourceforge.net/>
+ * Copyright (C) 2010-2013 SIPE Project <http://sipe.sourceforge.net/>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -173,13 +173,6 @@ static void sipe_make_signature(struct sipe_core_private *sipe_private,
 	}
 }
 
-static gchar *auth_header_version(struct sip_auth *auth)
-{
-	return(auth->version > 2 ?
-	       g_strdup_printf(", version=%d", auth->version) :
-	       g_strdup(""));
-}
-
 static const gchar *const auth_type_to_protocol[] = {
 	NULL,       /* SIPE_AUTHENTICATION_TYPE_UNSET     */
 	"NTLM",     /* SIPE_AUTHENTICATION_TYPE_NTLM      */
@@ -318,7 +311,13 @@ static gchar *initialize_auth_context(struct sipe_core_private *sipe_private,
 	}
 
 	opaque_str = auth->opaque ? g_strdup_printf(", opaque=\"%s\"", auth->opaque) : g_strdup("");
-	version_str = auth_header_version(auth);
+
+	if (auth->version > 2) {
+		version_str = g_strdup_printf(", version=%d", auth->version);
+	} else {
+		version_str = g_strdup("");
+	}
+
 	ret = g_strdup_printf("%s qop=\"auth\"%s, realm=\"%s\", targetname=\"%s\"%s%s%s",
 			      auth->protocol, opaque_str,
 			      auth->realm, auth->target,
@@ -328,17 +327,6 @@ static gchar *initialize_auth_context(struct sipe_core_private *sipe_private,
 	g_free(gssapi_str);
 	g_free(sign_str);
 
-	return(ret);
-}
-
-static gchar *start_auth_handshake(struct sip_auth *auth)
-{
-	gchar *version_str = auth_header_version(auth);
-	gchar *ret = g_strdup_printf("%s qop=\"auth\", realm=\"%s\", targetname=\"%s\", gssapi-data=\"\"%s",
-				     auth->protocol,
-				     auth->realm, auth->target,
-				     version_str);
-	g_free(version_str);
 	return(ret);
 }
 
@@ -355,16 +343,6 @@ static gchar *auth_header(struct sipe_core_private *sipe_private,
 	 */
 	if (msg->signature) {
 		ret = msg_signature_to_auth(auth, msg);
-
-	/*
-	 * If the message isn't signed then we don't have a initialized
-         * authentication context yet.
-	 *
-	 * Start the authentication handshake if NTLM is selected.
-	 */
-	} else if ((auth->type == SIPE_AUTHENTICATION_TYPE_NTLM) &&
-		   !auth->gssapi_data) {
-		ret = start_auth_handshake(auth);
 
 	/*
 	 * We should reach this point only when the authentication context

commit 47b0c9e6553a941cc2db277b48ad9a0940f7d6ac

security: fix TLS-DSK authentication
Commit 5fdf3728c82a23b07e7eeb80f9c1d4cee38f1511 broke TLS-DSK
authentication. Make sure we only return a special string for
gssapi_data when really requested, i.e. for the initial message in
connection-less NTLM.

src/core/sip-sec-ntlm.c
@@ -1742,8 +1742,9 @@ sip_sec_init_sec_context__ntlm(SipSecContext context,
 			sip_sec_ntlm_message_describe(out_buff, "Negotiate");
 		/* SIP */
 		} else {
+			/* empty initial message for connection-less NTLM */
 			out_buff->length = 0;
-			out_buff->value = NULL;
+			out_buff->value = (guint8 *) g_strdup("");
 		}
 	} else 	{
 		sip_uint32 res;

src/core/sip-sec-sspi.c
@@ -156,7 +156,7 @@ sip_sec_init_sec_context__sspi(SipSecContext context,
 		if (in_buff.value == NULL) {
 			SIPE_DEBUG_INFO_NOFORMAT("sip_sec_init_sec_context__sspi: initial message for connection-less NTLM");
 			out_buff->length = 0;
-			out_buff->value = NULL;
+			out_buff->value = (guint8 *) g_strdup("");
 			return SIP_SEC_E_OK;
 
 		/* call again to create context for connection-less NTLM */

src/core/sip-sec.c
@@ -159,10 +159,14 @@ sip_sec_init_context_step(SipSecContext context,
 
 		if (ret == SIP_SEC_E_OK) {
 
-			if (out_buff.length > 0 && out_buff.value) {
-				*output_toked_base64 = g_base64_encode(out_buff.value, out_buff.length);
-			} else {
-				*output_toked_base64 = g_strdup("");
+			if (out_buff.value) {
+				if (out_buff.length > 0) {
+					*output_toked_base64 = g_base64_encode(out_buff.value, out_buff.length);
+				} else {
+					/* special string: caller takes ownership */
+					*output_toked_base64 = (gchar *) out_buff.value;
+					out_buff.value = NULL;
+				}
 			}
 
 			g_free(out_buff.value);