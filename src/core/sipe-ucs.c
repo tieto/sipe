206f09f9c10cdcf07022eeb92d27db3053646cf2
Stefan Becker <chemobejk@gmail.com>
2013-08-07 19:14:16 +0300

    ucs: trigger EWS auto discover on initialization
    We need the EWS URL for UCS.

--- src/core/sipe-ucs.c
+++ src/core/sipe-ucs.c
@@ -27,13 +27,36 @@
 
 #include <glib.h>
 
+#include "sipe-backend.h"
+#include "sipe-common.h"
 #include "sipe-core.h"
 #include "sipe-core-private.h"
+#include "sipe-ews-autodiscover.h"
 #include "sipe-ucs.h"
+#include "sipe-utils.h"
 
 struct sipe_ucs {
-	guint dummy;
+	gchar *ews_url;
 };
+
+static void ucs_ews_autodiscover_cb(struct sipe_core_private *sipe_private,
+				    const struct sipe_ews_autodiscover_data *ews_data,
+				    SIPE_UNUSED_PARAMETER gpointer callback_data)
+{
+	struct sipe_ucs *ucs = sipe_private->ucs;
+	const gchar *ews_url = ews_data->ews_url;
+
+	if (!ucs)
+		return;
+
+	if (is_empty(ews_url)) {
+		SIPE_DEBUG_ERROR_NOFORMAT("ucs_ews_autodiscover_cb: can't detect EWS URL, contact list operations will not work!");
+		return;
+	}
+
+	SIPE_DEBUG_INFO("ucs_ews_autodiscover_cb: EWS URL '%s'", ews_url);
+	ucs->ews_url = g_strdup(ews_url);
+}
 
 void sipe_ucs_init(struct sipe_core_private *sipe_private)
 {
@@ -41,6 +64,10 @@ void sipe_ucs_init(struct sipe_core_private *sipe_private)
 		return;
 
 	sipe_private->ucs = g_new0(struct sipe_ucs, 1);
+
+	sipe_ews_autodiscover_start(sipe_private,
+				    ucs_ews_autodiscover_cb,
+				    NULL);
 }
 
 void sipe_ucs_free(struct sipe_core_private *sipe_private)
@@ -50,6 +77,7 @@ void sipe_ucs_free(struct sipe_core_private *sipe_private)
 	if (!ucs)
 		return;
 
+	g_free(ucs->ews_url);
 	g_free(ucs);
 	sipe_private->ucs = NULL;
 }
