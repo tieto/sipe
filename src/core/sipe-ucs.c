e91c23368f40f9414cde460419beb62d95dc4dce
Stefan Becker <chemobejk@gmail.com>
2013-08-08 17:24:35 +0300

    ucs: send GetImItemList request
    The response will contain the contact list as stored in UCS. We will
    need this to get access to the group/buddy IDs for future requests.

--- src/core/sipe-ucs.c
+++ src/core/sipe-ucs.c
@@ -25,6 +25,8 @@
  *  <http://msdn.microsoft.com/en-us/library/jj194130.aspx>
  */
 
+#include <string.h>
+
 #include <glib.h>
 
 #include "sipe-backend.h"
@@ -32,12 +34,85 @@
 #include "sipe-core.h"
 #include "sipe-core-private.h"
 #include "sipe-ews-autodiscover.h"
+#include "sipe-http.h"
 #include "sipe-ucs.h"
 #include "sipe-utils.h"
+#include "sipe-xml.h"
 
 struct sipe_ucs {
 	gchar *ews_url;
+	struct sipe_http_request *request;
 };
+
+typedef void (sipe_ucs_callback)(struct sipe_core_private *sipe_private,
+				 const sipe_xml *xml);
+
+static void ucs_process_get_im_item_list_response(struct sipe_core_private *sipe_private,
+						  const sipe_xml *xml)
+{
+	/* temporary */
+	(void)sipe_private;
+	(void)xml;
+}
+
+static void ucs_response(struct sipe_core_private *sipe_private,
+			 guint status,
+			 SIPE_UNUSED_PARAMETER GSList *headers,
+			 const gchar *body,
+			 gpointer data)
+{
+	struct sipe_ucs *ucs = sipe_private->ucs;
+
+	if (!ucs)
+		return;
+
+	ucs->request = NULL;
+
+	if ((status == SIPE_HTTP_STATUS_OK) && body) {
+		sipe_xml *xml = sipe_xml_parse(body, strlen(body));
+		sipe_ucs_callback *callback = data;
+
+		SIPE_DEBUG_INFO_NOFORMAT("ucs_response: received valid SOAP response");
+		(*callback)(sipe_private, xml);
+
+		sipe_xml_free(xml);
+	}
+}
+
+static void ucs_request(struct sipe_core_private *sipe_private,
+			const gchar *body,
+			sipe_ucs_callback *callback)
+{
+	struct sipe_ucs *ucs = sipe_private->ucs;
+	const gchar *soap = g_strdup_printf("<?xml version=\"1.0\"?>\r\n"
+					    "<soap:Envelope"
+					    " xmlns:m=\"http://schemas.microsoft.com/exchange/services/2006/messages\""
+					    " xmlns:soap=\"http://schemas.xmlsoap.org/soap/envelope/\""
+					    " xmlns:t=\"http://schemas.microsoft.com/exchange/services/2006/types\""
+					    " >"
+					    " <soap:Header>"
+					    "  <t:RequestServerVersion Version=\"Exchange2013\" />"
+					    " </soap:Header>"
+					    " <soap:Body>"
+					    "  %s"
+					    " </soap:Body>"
+					    "</soap:Envelope>",
+					    body);
+
+	ucs->request = sipe_http_request_post(sipe_private,
+					      ucs->ews_url,
+					      NULL,
+					      soap,
+					      "text/xml; charset=UTF-8",
+					      ucs_response,
+					      callback);
+	if (ucs->request) {
+		sipe_core_email_authentication(sipe_private,
+					       ucs->request);
+		sipe_http_request_allow_redirect(ucs->request);
+		sipe_http_request_ready(ucs->request);
+	}
+}
 
 static void ucs_ews_autodiscover_cb(struct sipe_core_private *sipe_private,
 				    const struct sipe_ews_autodiscover_data *ews_data,
@@ -56,6 +131,10 @@ static void ucs_ews_autodiscover_cb(struct sipe_core_private *sipe_private,
 
 	SIPE_DEBUG_INFO("ucs_ews_autodiscover_cb: EWS URL '%s'", ews_url);
 	ucs->ews_url = g_strdup(ews_url);
+
+	ucs_request(sipe_private,
+		    "<m:GetImItemList/>",
+		    ucs_process_get_im_item_list_response);
 }
 
 void sipe_ucs_init(struct sipe_core_private *sipe_private)
@@ -77,6 +156,8 @@ void sipe_ucs_free(struct sipe_core_private *sipe_private)
 	if (!ucs)
 		return;
 
+	if (ucs->request)
+		sipe_http_request_cancel(ucs->request);
 	g_free(ucs->ews_url);
 	g_free(ucs);
 	sipe_private->ucs = NULL;
