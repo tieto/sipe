a72d332cc2fc6b67e51c51f74b6c023b10d8ea88
Youness Alaoui <kakaroto@kakaroto.homelinux.net>
2014-07-21 16:56:31 -0400

    media: add application type

--- src/core/sipe-media.c
+++ src/core/sipe-media.c
@@ -406,6 +406,8 @@ media_stream_to_sdpmedia(struct sipe_media_call_private *call_private,
 		type = SIPE_MEDIA_VIDEO;
 	else if (sipe_strequal(sdpmedia->name, "data"))
 		type = SIPE_MEDIA_APPLICATION;
+	else if (sipe_strequal(sdpmedia->name, "applicationsharing"))
+		type = SIPE_MEDIA_APPLICATION;
 	else {
 		// TODO: incompatible media, should not happen here
 		g_free(sdpmedia->name);
@@ -1382,6 +1384,8 @@ process_incoming_invite_call(struct sipe_core_private *sipe_private,
 			else if (sipe_strequal(id, "video"))
 				type = SIPE_MEDIA_VIDEO;
 			else if (sipe_strequal(id, "data"))
+				type = SIPE_MEDIA_APPLICATION;
+			else if (sipe_strequal(id, "applicationsharing"))
 				type = SIPE_MEDIA_APPLICATION;
 			else
 				continue;
