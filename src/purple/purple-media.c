cf219519092a2288df11137239429b933cba6bc8
Jakub Adam <jakub.adam@ktknet.cz>
2014-10-21 08:40:06 +0200

    media: obey port range settings from the server
    So far libnice was picking listen ports at random from the full
    available range, which doesn't have to play well with firewalls along
    the way. Lync administrator can restrict which ports the clients are
    meant to use and Sipe should respect that setting as well.

--- src/purple/purple-media.c
+++ src/purple/purple-media.c
@@ -527,13 +527,14 @@ sipe_backend_media_add_stream(struct sipe_media_call *call,
 			      SipeMediaType type,
 			      SipeIceVersion ice_version,
 			      gboolean initiator,
-			      struct sipe_backend_media_relays *media_relays)
+			      struct sipe_backend_media_relays *media_relays,
+			      guint min_port, guint max_port)
 {
 	struct sipe_backend_media *media = call->backend_private;
 	struct sipe_backend_media_stream *stream = NULL;
 	PurpleMediaSessionType prpl_type = sipe_media_to_purple(type);
 	// Preallocate enough space for all potential parameters to fit.
-	GParameter *params = g_new0(GParameter, 5);
+	GParameter *params = g_new0(GParameter, 6);
 	guint params_cnt = 0;
 	gchar *transmitter;
 	GValue *relay_info = NULL;
@@ -553,6 +554,20 @@ sipe_backend_media_add_stream(struct sipe_media_call *call,
 				 NICE_COMPATIBILITY_OC2007 :
 				 NICE_COMPATIBILITY_OC2007R2);
 		++params_cnt;
+
+		if (min_port != 0) {
+			params[params_cnt].name = "min-port";
+			g_value_init(&params[params_cnt].value, G_TYPE_UINT);
+			g_value_set_uint(&params[params_cnt].value, min_port);
+			++params_cnt;
+		}
+
+		if (max_port != 0) {
+			params[params_cnt].name = "max-port";
+			g_value_init(&params[params_cnt].value, G_TYPE_UINT);
+			g_value_set_uint(&params[params_cnt].value, max_port);
+			++params_cnt;
+		}
 
 		if (media_relays) {
 			params[params_cnt].name = "relay-info";
