a399765e5d49950fd9336346fcfa34415a63cd84
Stefan Becker <chemobejk@gmail.com>
2014-10-27 09:43:06 +0200

    mingw: update fetch script to Pidgin 2.10.10

--- contrib/mingw-cross-compile/fetch.sh
+++ contrib/mingw-cross-compile/fetch.sh
@@ -11,7 +11,7 @@
 # Check these page for latest MinGW/Pidgin URLs if you get fetch errors!
 #
 # update Pidgin version here
-export PIDGIN_VERSION=2.10.9
+export PIDGIN_VERSION=2.10.10
 
 # must be absolute path
 export PIDGIN_DEV_ROOT=$(pwd -P)/build-${PIDGIN_VERSION}
@@ -31,20 +31,20 @@ mkdir -p ${MINGW_DIR}
 echo 1>&2 fetching mingw...
 cd ${SOURCES_DIR}
 #wget -nv http://sourceforge.net/projects/mingw/files/MinGW/Base/binutils-2.20/binutils-2.20-1-mingw32-bin.tar.gz
-wget -nv http://sourceforge.net/projects/mingw/files/MinGW/Base/mingw-rt/mingwrt-3.17/mingwrt-3.17-mingw32-dev.tar.gz
-wget -nv http://sourceforge.net/projects/mingw/files/MinGW/Base/mingw-rt/mingwrt-3.17/mingwrt-3.17-mingw32-dll.tar.gz
-wget -nv http://sourceforge.net/projects/mingw/files/MinGW/Base/w32api/w32api-3.14/w32api-3.14-mingw32-dev.tar.gz
-wget -nv http://sourceforge.net/projects/mingw/files/MinGW/Base/gcc/Version4/Previous%20Release%20gcc-4.4.0/gmp-4.2.4-mingw32-dll.tar.gz
-wget -nv http://sourceforge.net/projects/mingw/files/MinGW/Base/libiconv/libiconv-1.13.1-1/libiconv-1.13.1-1-mingw32-dll-2.tar.lzma
-wget -nv http://sourceforge.net/projects/mingw/files/MinGW/Base/gcc/Version4/Previous%20Release%20gcc-4.4.0/mpfr-2.4.1-mingw32-dll.tar.gz
-wget -nv http://sourceforge.net/projects/mingw/files/MinGW/Base/gcc/Version4/Previous%20Release%20gcc-4.4.0/pthreads-w32-2.8.0-mingw32-dll.tar.gz
-wget -nv http://sourceforge.net/projects/mingw/files/MinGW/Base/gcc/Version4/Previous%20Release%20gcc-4.4.0/gcc-core-4.4.0-mingw32-bin.tar.gz
-wget -nv http://sourceforge.net/projects/mingw/files/MinGW/Base/gcc/Version4/Previous%20Release%20gcc-4.4.0/gcc-core-4.4.0-mingw32-dll.tar.gz
+wget -nv http://sourceforge.net/projects/mingw/files/MinGW/Base/mingw-rt/mingwrt-3.20/mingwrt-3.20-mingw32-dev.tar.gz
+wget -nv http://sourceforge.net/projects/mingw/files/MinGW/Base/mingw-rt/mingwrt-3.20/mingwrt-3.20-mingw32-dll.tar.gz
+wget -nv http://sourceforge.net/projects/mingw/files/MinGW/Base/w32api/w32api-3.17/w32api-3.17-2-mingw32-dev.tar.lzma
+wget -nv http://sourceforge.net/projects/mingw/files/MinGW/Base/gmp/gmp-5.0.1-1/gmp-5.0.1-1-mingw32-dev.tar.lzma
+wget -nv http://sourceforge.net/projects/mingw/files/MinGW/Base/libiconv/libiconv-1.14-2/libiconv-1.14-2-mingw32-dev.tar.lzma
+wget -nv http://sourceforge.net/projects/mingw/files/MinGW/Base/mpfr/mpfr-2.4.1-1/mpfr-2.4.1-1-mingw32-dev.tar.lzma
+wget -nv http://sourceforge.net/projects/mingw/files/MinGW/Base/pthreads-w32/pthreads-w32-2.9.0-pre-20110507-2/pthreads-w32-2.9.0-mingw32-pre-20110507-2-dev.tar.lzma
+wget -nv http://sourceforge.net/projects/mingw/files/MinGW/Base/gcc/Version4/gcc-4.7.2-1/gcc-core-4.7.2-1-mingw32-bin.tar.lzma
+wget -nv http://sourceforge.net/projects/mingw/files/MinGW/Base/gcc/Version4/gcc-4.7.2-1/libgcc-4.7.2-1-mingw32-dll-1.tar.lzma
 
 echo 1>&2 unpacking mingw...
 cd ${MINGW_DIR}
 for file in ${SOURCES_DIR}/*tar.gz ; do tar xzf ${file} ; done
-tar xf ${SOURCES_DIR}/libiconv-1.13.1-1-mingw32-dll-2.tar.lzma
+for file in ${SOURCES_DIR}/*tar.lzma ; do tar xf ${file} ; done
 
 echo 1>&2 fetching pidgin dev stuff...
 cd ${SOURCES_DIR}
@@ -56,7 +56,7 @@ wget -nv http://ftp.gnome.org/pub/gnome/binaries/win32/dependencies/libxml2_2.9.
 wget -nv https://developer.pidgin.im/static/win32/tcl-8.4.5.tar.gz
 wget -nv https://developer.pidgin.im/static/win32/gtkspell-2.0.16.tar.bz2
 wget -nv https://developer.pidgin.im/static/win32/enchant_1.6.0_win32.zip
-wget -nv https://developer.pidgin.im/static/win32/nss-3.15.4-nspr-4.10.2.tar.gz
+wget -nv https://developer.pidgin.im/static/win32/nss-3.17.1-nspr-4.10.7.tar.gz
 wget -nv https://developer.pidgin.im/static/win32/silc-toolkit-1.1.10.tar.gz
 wget -nv https://developer.pidgin.im/static/win32/meanwhile-1.0.2_daa3-win32.zip
 wget -nv https://developer.pidgin.im/static/win32/cyrus-sasl-2.1.25.tar.gz
@@ -76,8 +76,7 @@ unzip ${SOURCES_DIR}/intltool_0.40.4-1_win32.zip -d ${DEV_DIR}/intltool_0.40.4-1
 cd ${DEV_DIR}
 tar xzf ${SOURCES_DIR}/tcl-8.4.5.tar.gz
 tar xjf ${SOURCES_DIR}/gtkspell-2.0.16.tar.bz2
-# Doesn't seem to be compressed, despite the name...
-tar xf  ${SOURCES_DIR}/nss-3.15.4-nspr-4.10.2.tar.gz
+tar xzf ${SOURCES_DIR}/nss-3.17.1-nspr-4.10.7.tar.gz
 tar xzf ${SOURCES_DIR}/silc-toolkit-1.1.10.tar.gz
 tar xzf ${SOURCES_DIR}/cyrus-sasl-2.1.25.tar.gz
 
