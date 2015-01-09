7753ee4bc891b766521e0412a8d93a3aa2b53aa5
Stefan Becker <chemobejk@gmail.com>
2015-01-10 00:09:51 +0200

    Feature #79: support for Adium group chat bookmarks

--- src/adium/ESPurpleSIPEAccount.m
+++ src/adium/ESPurpleSIPEAccount.m
@@ -277,4 +277,17 @@
 }
 
 
+// generate group chat's creation dictionary from purple conversation, e.g. for bookmarking
+- (NSDictionary *)extractChatCreationDictionaryFromConversation:(PurpleConversation *)conv
+{
+	NSMutableDictionary *dict = [NSMutableDictionary dictionary];
+	PurpleConnection* gc = purple_account_get_connection([self purpleAccount]);
+    const gchar *uri = sipe_core_chat_id(PURPLE_GC_TO_SIPE_CORE_PUBLIC,
+                                         (struct sipe_chat_session *) conv);
+
+    [dict setObject:[NSString stringWithUTF8String:uri] forKey:@"uri"];
+
+	return dict;
+}
+
 @end
