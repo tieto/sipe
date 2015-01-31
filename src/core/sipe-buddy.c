tieto/sipe

commit 3d163da0b3086863304b95f0509040f3d994b03e

buddy: fix missing i18n of "Mobile" string
Also optimize code by using a GString to compile the result.

This fixes commit 949b5743a27579cf9850cf95c9f8058b6967d8bd

src/core/sipe-buddy.c
@@ -3,7 +3,7 @@
  *
  * pidgin-sipe
  *
- * Copyright (C) 2010-2013 SIPE Project <http://sipe.sourceforge.net/>
+ * Copyright (C) 2010-2014 SIPE Project <http://sipe.sourceforge.net/>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -519,45 +519,30 @@ gchar *sipe_core_buddy_status(struct sipe_core_public *sipe_public,
 			      const gchar *status_text)
 {
 	struct sipe_buddy *sbuddy;
-	const char *activity_str;
-
-	gchar *tmp = NULL;
-	gchar *status = NULL;
+	GString *status;
 
 	if (!sipe_public) return NULL; /* happens on pidgin exit */
 
 	sbuddy = sipe_buddy_find_by_uri(SIPE_CORE_PRIVATE, uri);
 	if (!sbuddy) return NULL;
 
-	activity_str = sbuddy->activity ? sbuddy->activity :
-		(activity == SIPE_ACTIVITY_BUSY) || (activity == SIPE_ACTIVITY_BRB) ?
-		status_text : NULL;
-
-	if (activity_str) {
-		status = g_strdup(activity_str);
-   	}
+	status = g_string_new(sbuddy->activity ? sbuddy->activity :
+			      (activity == SIPE_ACTIVITY_BUSY) || (activity == SIPE_ACTIVITY_BRB) ?
+			      status_text : NULL);
 
 	if (sbuddy->is_mobile) {
-		if (status) {
-			tmp = status;
-			status = g_strdup_printf("%s - %s", status, "Mobile");
-			g_free(tmp);
-		} else {
-			status = g_strdup("Mobile");
-		}
+		if (status->len)
+			g_string_append(status, " - ");
+		g_string_append(status, _("Mobile"));
 	}
 
 	if (sbuddy->note) {
-		if (status) {
-			tmp = status;
-			status = g_strdup_printf("%s - %s", status, sbuddy->note);
-			g_free(tmp);
-		} else {
-			status = g_strdup(sbuddy->note);
-		}
+		if (status->len)
+			g_string_append(status, " - ");
+		g_string_append(status, sbuddy->note);
 	}
 
-	return status;
+	return(g_string_free(status, FALSE));
 }
 
 gchar *sipe_buddy_get_alias(struct sipe_core_private *sipe_private,

commit 7d706b3b2a34f9a51ec29518d2c0cb345575a120

Fix #277: Raised contact names
This fixes a regression introduced by

  commit 3d163da0b3086863304b95f0509040f3d994b03e

Pidgin considers an empty string as a valid status text. Therefore
sipe_core_buddy_status() must return NULL instead.

src/core/sipe-buddy.c
@@ -3,7 +3,7 @@
  *
  * pidgin-sipe
  *
- * Copyright (C) 2010-2014 SIPE Project <http://sipe.sourceforge.net/>
+ * Copyright (C) 2010-2015 SIPE Project <http://sipe.sourceforge.net/>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -542,7 +542,8 @@ gchar *sipe_core_buddy_status(struct sipe_core_public *sipe_public,
 		g_string_append(status, sbuddy->note);
 	}
 
-	return(g_string_free(status, FALSE));
+	/* return NULL instead of empty status text */
+	return(g_string_free(status, status->len ? FALSE : TRUE));
 }
 
 gchar *sipe_buddy_get_alias(struct sipe_core_private *sipe_private,