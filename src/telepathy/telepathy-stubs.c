ddb21b812b885c6acbe11da222c6abbaf79499e9
Stefan Becker <chemobejk@gmail.com>
2015-09-08 08:33:03 +0300

    telepathy: add missing filetransfer stubs

--- src/telepathy/telepathy-stubs.c
+++ src/telepathy/telepathy-stubs.c
@@ -125,7 +125,14 @@ void sipe_backend_chat_topic(SIPE_UNUSED_PARAMETER struct sipe_backend_chat_sess
 
 void sipe_backend_ft_error(SIPE_UNUSED_PARAMETER struct sipe_file_transfer *ft,
 			   SIPE_UNUSED_PARAMETER const gchar *errmsg) {}
+const gchar *sipe_backend_ft_get_error(SIPE_UNUSED_PARAMETER struct sipe_file_transfer *ft) { return(""); }
 void sipe_backend_ft_deallocate(SIPE_UNUSED_PARAMETER struct sipe_file_transfer *ft) {}
+gssize sipe_backend_ft_read(SIPE_UNUSED_PARAMETER struct sipe_file_transfer *ft,
+			    SIPE_UNUSED_PARAMETER guchar *data,
+			    SIPE_UNUSED_PARAMETER gsize size) { return(-1); }
+gssize sipe_backend_ft_write(SIPE_UNUSED_PARAMETER struct sipe_file_transfer *ft,
+			     SIPE_UNUSED_PARAMETER const guchar *data,
+			     SIPE_UNUSED_PARAMETER gsize size) { return(-1); }
 void sipe_backend_ft_cancel_local(SIPE_UNUSED_PARAMETER struct sipe_file_transfer *ft) {}
 void sipe_backend_ft_cancel_remote(SIPE_UNUSED_PARAMETER struct sipe_file_transfer *ft) {}
 void sipe_backend_ft_incoming(SIPE_UNUSED_PARAMETER struct sipe_core_public *sipe_public,
