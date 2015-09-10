c53c77a2cd93afde9f7b03ee0161b75ad0b60bf9
Jakub Adam <jakub.adam@ktknet.cz>
2015-09-10 08:54:50 +0200

    buddy: disallow sharing desktop multiple times with the same user

--- src/core/sipe-buddy.c
+++ src/core/sipe-buddy.c
@@ -45,10 +45,12 @@
 #include "sipe-conf.h"
 #include "sipe-core.h"
 #include "sipe-core-private.h"
+#include "sipe-appshare.h"
 #include "sipe-digest.h"
 #include "sipe-group.h"
 #include "sipe-http.h"
 #include "sipe-im.h"
+#include "sipe-media.h"
 #include "sipe-nls.h"
 #include "sipe-ocs2005.h"
 #include "sipe-ocs2007.h"
@@ -2241,6 +2243,27 @@ static struct sipe_backend_buddy_menu *buddy_menu_phone(struct sipe_core_public 
 	return(menu);
 }
 
+#ifdef HAVE_RDP_SERVER
+static struct sipe_backend_buddy_menu *buddy_menu_share_desktop(struct sipe_core_public *sipe_public,
+								struct sipe_backend_buddy_menu *menu,
+								const gchar *buddy_name)
+{
+	struct sipe_media_call *call;
+
+	call = sipe_media_call_find(SIPE_CORE_PRIVATE, buddy_name);
+	if (call && sipe_appshare_get_role(call) == SIPE_APPSHARE_ROLE_PRESENTER) {
+		/* We're already presenting to this buddy. */
+		return menu;
+	}
+
+	return sipe_backend_buddy_menu_add(sipe_public,
+					   menu,
+					   _("Share my desktop"),
+					   SIPE_BUDDY_MENU_SHARE_DESKTOP,
+					   NULL);
+}
+#endif // HAVE_RDP_SERVER
+
 struct sipe_backend_buddy_menu *sipe_core_buddy_create_menu(struct sipe_core_public *sipe_public,
 							    const gchar *buddy_name,
 							    struct sipe_backend_buddy_menu *menu)
@@ -2371,11 +2394,7 @@ struct sipe_backend_buddy_menu *sipe_core_buddy_create_menu(struct sipe_core_pub
 	}
 
 #ifdef HAVE_RDP_SERVER
-	menu = sipe_backend_buddy_menu_add(sipe_public,
-					   menu,
-					   _("Share my desktop"),
-					   SIPE_BUDDY_MENU_SHARE_DESKTOP,
-					   NULL);
+	menu = buddy_menu_share_desktop(sipe_public, menu, buddy_name);
 #endif // HAVE_RDP_SERVER
 
 	/* access level control */
