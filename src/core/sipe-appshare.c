f18fcc3555090f02b95b45397cb4e2a2e8ae4c63
Jakub Adam <jakub.adam@ktknet.cz>
2016-05-13 10:23:03 +0200

    media: make stream data accessible to SIPE backend

--- src/core/sipe-appshare.c
+++ src/core/sipe-appshare.c
@@ -256,7 +256,9 @@ static void
 confirmed_cb(SIPE_UNUSED_PARAMETER struct sipe_media_call *call,
 	     struct sipe_media_stream *stream)
 {
-	struct sipe_appshare *appshare = sipe_media_stream_get_data(stream);
+	struct sipe_appshare *appshare;
+
+	appshare = sipe_core_media_stream_get_data(stream);
 	appshare->confirmed = TRUE;
 
 	if (appshare->writable && appshare->confirmed && !appshare->socket) {
@@ -330,9 +332,11 @@ rdp_channel_writable_cb(SIPE_UNUSED_PARAMETER GIOChannel *channel,
 static void
 read_cb(struct sipe_media_stream *stream)
 {
-	struct sipe_appshare *appshare = sipe_media_stream_get_data(stream);
+	struct sipe_appshare *appshare;
 	gint bytes_read = 0;
 	gssize bytes_written = 0;
+
+	appshare = sipe_core_media_stream_get_data(stream);
 
 	if (appshare->rdp_channel_writable_watch_id != 0) {
 		/* Data still in the buffer. Let the client read it first. */
@@ -379,7 +383,9 @@ read_cb(struct sipe_media_stream *stream)
 static void
 writable_cb(struct sipe_media_stream *stream)
 {
-	struct sipe_appshare *appshare = sipe_media_stream_get_data(stream);
+	struct sipe_appshare *appshare;
+
+	appshare = sipe_core_media_stream_get_data(stream);
 	appshare->writable = TRUE;
 
 	if (appshare->writable && appshare->confirmed && !appshare->socket) {
@@ -618,7 +624,7 @@ candidate_pairs_established_cb(struct sipe_media_stream *stream)
 
 	g_return_if_fail(sipe_strequal(stream->id, "applicationsharing"));
 
-	appshare = sipe_media_stream_get_data(stream);
+	appshare = sipe_core_media_stream_get_data(stream);
 	if (appshare->server) {
 
 		// Shadow server has already been initialized.
@@ -817,7 +823,7 @@ sipe_core_applicationsharing_is_presenting(struct sipe_media_call *call)
 	if (stream) {
 		struct sipe_appshare *appshare;
 
-		appshare = sipe_media_stream_get_data(stream);
+		appshare = sipe_core_media_stream_get_data(stream);
 		return appshare && appshare->server;
 	}
 
