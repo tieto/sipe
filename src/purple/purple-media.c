0bcb160ba78905a0f9c3ceba5bcfb46c08521f7a
Jakub Adam <jakub.adam@ktknet.cz>
2016-05-24 12:06:01 +0200

    purple: apply manually assigned SSRC to backend
    H264 video requires stream SSRC in RTP frames match the value in SDP
    signalling message on SIP level.

--- src/purple/purple-media.c
+++ src/purple/purple-media.c
@@ -70,6 +70,7 @@ struct sipe_backend_media {
 	 * Number of media streams that were not yet locally accepted or rejected.
 	 */
 	guint unconfirmed_streams;
+	guint ssrc_to_assign;
 };
 
 struct sipe_backend_media_stream {
@@ -638,6 +639,7 @@ find_sinkpad(GValue *value, GstPad *fssession_sinkpad)
 static void
 gst_bus_cb(GstBus *bus, GstMessage *msg, struct sipe_media_stream *stream)
 {
+	PurpleMedia *m;
 	const GstStructure *s;
 	FsSession *fssession;
 	GstElement *tee;
@@ -649,6 +651,8 @@ gst_bus_cb(GstBus *bus, GstMessage *msg, struct sipe_media_stream *stream)
 		return;
 	}
 
+	m = stream->call->backend_private->m;
+
 	s = gst_message_get_structure(msg);
 	if (!gst_structure_has_name(s, "farstream-codecs-changed")) {
 		return;
@@ -657,8 +661,7 @@ gst_bus_cb(GstBus *bus, GstMessage *msg, struct sipe_media_stream *stream)
 	fssession = g_value_get_object(gst_structure_get_value(s, "session"));
 	g_return_if_fail(fssession);
 
-	tee = purple_media_get_tee(stream->call->backend_private->m, stream->id,
-				   NULL);
+	tee = purple_media_get_tee(m, stream->id, NULL);
 	g_return_if_fail(tee);
 
 	g_object_get(fssession, "sink-pad", &sinkpad, NULL);
@@ -670,19 +673,27 @@ gst_bus_cb(GstBus *bus, GstMessage *msg, struct sipe_media_stream *stream)
 	it = gst_element_iterate_src_pads(tee);
 	if (gst_iterator_find_custom(it, (GCompareFunc)find_sinkpad, &val,
 				     sinkpad)) {
-		GObject *rtpsession;
+		if (stream->ssrc_range) {
+			g_object_set(fssession, "ssrc",
+				     stream->ssrc_range->begin, NULL);
+		}
 
-		g_object_get(fssession, "internal-session", &rtpsession, NULL);
-		if (rtpsession) {
-			stream->backend_private->fssession =
-					gst_object_ref(fssession);
-			stream->backend_private->on_sending_rtcp_cb_id =
-					g_signal_connect(rtpsession,
-						"on-sending-rtcp",
-						G_CALLBACK(on_sending_rtcp_cb),
-						stream->backend_private);
+		if (purple_media_get_session_type(m, stream->id) == PURPLE_MEDIA_VIDEO) {
+			GObject *rtpsession;
 
-			g_object_unref (rtpsession);
+			g_object_get(fssession,
+				     "internal-session", &rtpsession, NULL);
+			if (rtpsession) {
+				stream->backend_private->fssession =
+						gst_object_ref(fssession);
+				stream->backend_private->on_sending_rtcp_cb_id =
+						g_signal_connect(rtpsession,
+							"on-sending-rtcp",
+							G_CALLBACK(on_sending_rtcp_cb),
+							stream->backend_private);
+
+				g_object_unref (rtpsession);
+			}
 		}
 
 		g_signal_handler_disconnect(bus,
@@ -781,7 +792,7 @@ sipe_backend_media_add_stream(struct sipe_media_stream *stream,
 	backend_stream->initialized_cb_was_fired = FALSE;
 
 	pipe = purple_media_manager_get_pipeline(purple_media_manager_get());
-	if (type == SIPE_MEDIA_VIDEO && pipe) {
+	if (pipe) {
 		GstBus *bus;
 
 		bus = gst_element_get_bus(pipe);
