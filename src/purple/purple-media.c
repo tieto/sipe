206fe857606282c791dfd9ccd49c9d551ea98188
Jakub Adam <jakub.adam@ktknet.cz>
2017-02-02 18:54:12 +0100

    media: fix no relay with purple 2.x & GStreamer 1.0
    Starting with version 2.10.12 it is possible to compile libpurple 2.x
    with GStreamer 1.0 and Farstream 0.2, which invalidates our check
    deciding whether to use GValueArray or GPtrArray as a data type of
    "relay-info" property.
    
    Moreover, because Sipe V&V has since 1.22.0 gained a build dependency
    on Farstream 0.2, we may as well do away with the legacy code and use
    GPtrArray everywhere now.

--- src/purple/purple-media.c
+++ src/purple/purple-media.c
@@ -84,12 +84,6 @@ struct sipe_backend_media_stream {
 	gulong on_sending_rtcp_cb_id;
 };
 
-#if PURPLE_VERSION_CHECK(3,0,0)
-#define SIPE_RELAYS_G_TYPE G_TYPE_PTR_ARRAY
-#else
-#define SIPE_RELAYS_G_TYPE G_TYPE_VALUE_ARRAY
-#endif
-
 void
 sipe_backend_media_stream_free(struct sipe_backend_media_stream *stream)
 {
@@ -442,17 +436,7 @@ append_relay(struct sipe_backend_media_relays *relay_info, const gchar *ip,
 			NULL);
 
 	if (gst_relay_info) {
-#if PURPLE_VERSION_CHECK(3,0,0)
 		g_ptr_array_add((GPtrArray *)relay_info, gst_relay_info);
-#else
-		GValue value;
-		memset(&value, 0, sizeof(GValue));
-		g_value_init(&value, GST_TYPE_STRUCTURE);
-		gst_value_set_structure(&value, gst_relay_info);
-
-		g_value_array_append((GValueArray *)relay_info, &value);
-		gst_structure_free(gst_relay_info);
-#endif
 	}
 }
 
@@ -462,11 +446,7 @@ sipe_backend_media_relays_convert(GSList *media_relays, gchar *username, gchar *
 	struct sipe_backend_media_relays *relay_info;
 
 	relay_info = (struct sipe_backend_media_relays *)
-#if PURPLE_VERSION_CHECK(3,0,0)
 			g_ptr_array_new_with_free_func((GDestroyNotify) gst_structure_free);
-#else
-			g_value_array_new(0);
-#endif
 
 	for (; media_relays; media_relays = media_relays->next) {\
 		struct sipe_media_relay *relay = media_relays->data;
@@ -496,11 +476,7 @@ sipe_backend_media_relays_convert(GSList *media_relays, gchar *username, gchar *
 void
 sipe_backend_media_relays_free(struct sipe_backend_media_relays *media_relays)
 {
-#if !PURPLE_VERSION_CHECK(3,0,0)
-	g_value_array_free((GValueArray *)media_relays);
-#else
 	g_ptr_array_unref((GPtrArray *)media_relays);
-#endif
 }
 
 #ifdef HAVE_XDATA
@@ -885,7 +861,7 @@ sipe_backend_media_add_stream(struct sipe_media_stream *stream,
 
 		if (media_relays) {
 			params[params_cnt].name = "relay-info";
-			g_value_init(&params[params_cnt].value, SIPE_RELAYS_G_TYPE);
+			g_value_init(&params[params_cnt].value, G_TYPE_PTR_ARRAY);
 			g_value_set_boxed(&params[params_cnt].value, media_relays);
 			relay_info = &params[params_cnt].value;
 			++params_cnt;
