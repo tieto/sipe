7c141007f1eb4d5f783c91def93aebcb0ad1884e
Stefan Becker <chemobejk@gmail.com>
2017-08-06 16:45:55 +0300

    purple: clean up purple-private.h
    Including purple-private.h should not have any dependencies outside
    glib.h.
    
    Unfortunately some types only work when the correct interface is
    included and thus they have to be included in purple-private.h. We could
    improve on that by splitting purple-private.h.

--- src/purple/purple-private.h
+++ src/purple/purple-private.h
@@ -3,7 +3,7 @@
  *
  * pidgin-sipe
  *
- * Copyright (C) 2010-2015 SIPE Project <http://sipe.sourceforge.net/>
+ * Copyright (C) 2010-2017 SIPE Project <http://sipe.sourceforge.net/>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -41,7 +41,12 @@
 
 #define SIPE_PURPLE_PROTOCOL_OPTIONS OPT_PROTO_CHAT_TOPIC | OPT_PROTO_PASSWORD_OPTIONAL
 
-#if !PURPLE_VERSION_CHECK(3,0,0)
+#if PURPLE_VERSION_CHECK(3,0,0)
+#include "conversationtypes.h" /* PurpleIMTypingState */
+#include "plugins.h"           /* PurplePlugin */
+#else
+#include "conversation.h"      /* PurpleTypingState */
+#include "plugin.h"            /* PurplePlugin */
 #define PurpleIMTypingState PurpleTypingState
 #define _PurpleProtocolAction _PurplePluginAction
 #endif
@@ -57,6 +62,7 @@ struct _PurpleConnection;
 struct _PurpleConversation;
 struct _PurpleGroup;
 struct _PurpleMessage;
+struct _PurpleNotifyUserInfo;
 struct _PurplePluginAction;
 struct _PurpleRoomlist;
 struct _PurpleStatus;
