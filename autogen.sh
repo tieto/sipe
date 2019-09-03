53a6a6463091bb3c54f96b18165caeb698f74b33
Stefan Becker <chemobejk@gmail.com>
2019-09-03 22:16:07 +0300

    configure: restore GITVERSION generation
    Too eager delete key...

--- autogen.sh
+++ autogen.sh
@@ -1,4 +1,18 @@
 #! /bin/sh
+###############################################################################
+# Generate GITVERSION
+###############################################################################
+_gitversion=$(git describe | grep -e -)
+if [ -n "${_gitversion}" ]; then
+	_gitversion=$(echo ${_gitversion} | cut -d- -f3 | sed 's/^g//')
+	echo -n ${_gitversion} >GITVERSION
+else
+	rm -f GITVERSION
+fi
+
+###############################################################################
+# Set up build from git tree
+###############################################################################
 set -e
 
 # Set up initial NLS stuff...
